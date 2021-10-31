7677cff3d8922a221df9352351e3410329413d08
romain <romain.crespel@gmail.com>
2021-10-31 11:46:04 +0100

    longbutton

--- QtWidgetsEx/LongPressButton.cpp
+++ QtWidgetsEx/LongPressButton.cpp
@@ -1,1 +1,33 @@
 #include "LongPressButton.h"
+#include <QMouseEvent>
+
+void LongPressButton::mousePressEvent(QMouseEvent* evt)
+{
+	if (evt->buttons() & Qt::LeftButton)
+	{
+		if (m_timerId == -1)
+		{
+			m_timerId = startTimer(std::chrono::milliseconds(30));
+		}
+		emit sg_pressed();
+	}
+	QPushButton::mouseReleaseEvent(evt);
+}
+
+void LongPressButton::mouseReleaseEvent(QMouseEvent* evt)
+{
+	if (m_timerId >= 0)
+	{
+		killTimer(m_timerId);
+		m_timerId = -1;
+	}
+	QPushButton::mouseReleaseEvent(evt);
+}
+
+void LongPressButton::timerEvent(QTimerEvent* evnt)
+{
+	if (evnt->timerId() == m_timerId)
+	{
+		m_timerId = startTimer(std::chrono::milliseconds(30));
+	}
+}
--- QtWidgetsEx/LongPressButton.h
+++ QtWidgetsEx/LongPressButton.h
@@ -3,8 +3,19 @@
 
 class LongPressButton : public QPushButton
 {
+	Q_OBJECT
 public:
 	using QPushButton::QPushButton;
-	// TODO
+
+signals:
+	void sg_pressed();
+
+protected:
+	void mousePressEvent(QMouseEvent* evt) final;
+	void mouseReleaseEvent(QMouseEvent* evt) final;
+	void timerEvent(QTimerEvent* evnt)final;
+
+private:
+	int m_timerId = -1;
 };
 
